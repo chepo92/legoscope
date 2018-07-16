47f7efa7bb8cd843ed61553a64185fbd800202ff
Axel <ansepulveda@uc.cl>
2018-07-16 03:10:25 -0400

    cleaning

--- Timelapse/camera_preview.py
+++ Timelapse/camera_preview.py
@@ -3,15 +3,16 @@ from time import sleep
 
 camera = PiCamera()
 
-
 # Minimal camera settings
 camera.resolution=(960,720)
-#camera.ISO=700
-camera.framerate = 30 # frames/sec, determines the max shutter speed
+#camera.framerate = 5 # frames/sec, determines the max shutter speed
+
+#camera.exposure_mode = 'off'
 #camera.shutter_speed = 500000 # exposure time in microsecs
-#camera.exposure_mode = 'off' #
-#camera.awb_gains = [1,1]
+
 #camera.awb_mode = 'off'
+#camera.awb_gains = [1,1]
+
 #camera.rotation = 90
 
 # alpha window transparency 
@@ -21,14 +22,18 @@ camera.framerate = 30 # frames/sec, determines the max shutter speed
 # Set ISO to the desired value
 camera.iso = 600
 # Wait for the automatic gain control to settle
-sleep(2)
-# Now fix the values
+sleep(5)
+
+# Query shutter speed value
 e = camera.exposure_speed
+# Now fix the ss
 camera.exposure_mode = 'off'
 camera.shutter_speed = e
 print('Shutter Speed: ' + str(e))
 
+# Query awb gains
 g = camera.awb_gains
+# Now fix the gains
 camera.awb_mode = 'off'
 camera.awb_gains = g
 print('AWB gains ' + str(g))
